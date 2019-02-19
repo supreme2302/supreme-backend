3b99859f188e8b5822192d66560f49c24f61b1e9
supreme2302 <chuvashov.egor@mail.ru>
2019-02-20 02:18:26 +0300

    add tests

--- src/main/java/com/supreme/spa/backend/vue/controller/UserController.java
+++ src/main/java/com/supreme/spa/backend/vue/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.websocket.server.PathParam;
 import java.awt.image.BufferedImage;
@@ -113,7 +114,7 @@ public class UserController {
     @PostMapping(path = "/logout")
     public ResponseEntity logout(HttpSession session) {
         if (session.getAttribute("user") == null) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body(new Message(UserStatus.ACCESS_ERROR));
         }
         session.invalidate();
@@ -165,7 +166,6 @@ public class UserController {
                     .body(new Message(UserStatus.NOT_UNIQUE_PHONE));
         }
         return ResponseEntity.ok(new Message(UserStatus.SUCCESSFULLY_CHANGED));
-
     }
 
 //    @GetMapping(path = "/list/{page}")
--- src/test/java/com/supreme/spa/backend/vue/VueApplicationTests.java
+++ src/test/java/com/supreme/spa/backend/vue/VueApplicationTests.java
@@ -1,103 +1,183 @@
 package com.supreme.spa.backend.vue;
-
 import com.google.gson.Gson;
+import com.supreme.spa.backend.vue.controller.UserController;
 import com.supreme.spa.backend.vue.models.Auth;
-import com.supreme.spa.backend.vue.services.UserService;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import com.supreme.spa.backend.vue.models.User;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.*;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.context.WebApplicationContext;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.nio.charset.Charset;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
+import java.util.UUID;
 
-import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.mockito.ArgumentMatchers.any;
-import static org.springframework.http.converter.json.Jackson2ObjectMapperBuilder.json;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc(print = MockMvcPrint.NONE)
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource("/application-test.properties")
 public class VueApplicationTests {
+
+    @Autowired
+    private UserController userController;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private UserService userService;
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    private static final HttpHeaders REQUEST_HEADERS = new HttpHeaders();
+    private Gson gson = new Gson();
 
     private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
             MediaType.APPLICATION_JSON.getSubtype(),
             Charset.forName("utf8"));
 
-    private Gson gson = new Gson();
 
-    public VueApplicationTests() {
 
-        final List<String> origin = Collections.singletonList("http://localhost:5002");
-        REQUEST_HEADERS.put(HttpHeaders.ORIGIN, origin);
-        final List<String> contentType = Collections.singletonList("application/json");
-        REQUEST_HEADERS.put(HttpHeaders.CONTENT_TYPE, contentType);
+
+    @Autowired
+    private WebApplicationContext wac;
+
+
+    @Before
+    public void setUp() throws Exception {
+
+//        mockSession = new MockHttpSession(wac.getServletContext(), UUID.randomUUID().toString());
     }
 
 
     @Test
-    public void registration() throws Exception {
-
-        ResultMatcher ok = status().isOk();
-        Auth testAuth = new Auth();
-        testAuth.setUsername("testAuthUser");
-        testAuth.setEmail("test@t.ruru");
-        testAuth.setPassword("123456789");
-        testAuth.setConfirmPassword("123456789");
-
-        String testAuthJson = gson.toJson(testAuth);
-        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/create")
+    @Sql(value = {"/test-set-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    public void registrationOkTest() throws Exception {
+        Auth auth = new Auth();
+        auth.setEmail("test@s.ru");
+        auth.setUsername("test");
+        auth.setPassword("123");
+        auth.setConfirmPassword("123");
+        String authJSON = gson.toJson(auth);
+        this.mockMvc.perform(post("/users/create")
                 .contentType(contentType)
-                .content(testAuthJson))
+                .content(authJSON))
+                .andDo(print())
                 .andExpect(status().isCreated());
-
-
     }
 
     @Test
-    public void testMock() {
-        Auth tAuth = new Auth();
-        tAuth.setEmail("a@test.test");
-        tAuth.setUsername("tessssssssssst");
-        tAuth.setPassword("12345678");
-        tAuth.setConfirmPassword("12345678");
-//        when(userService.testAuth(tAuth)).thenReturn(tAuth);
-        final HttpEntity<Auth> requestEntity = new HttpEntity<>(tAuth, REQUEST_HEADERS);
-        final ResponseEntity<String> response = restTemplate.postForEntity("/users/create", requestEntity, String.class);
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    @Sql(value = {"/test-set-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    public void registrationFailedConflictTest() throws Exception {
+        Auth auth = new Auth();
+        auth.setEmail("exist@e.ru");
+        auth.setUsername("exist");
+        auth.setPassword("123");
+        auth.setConfirmPassword("123");
+        String authJSON = gson.toJson(auth);
+        this.mockMvc.perform(post("/users/create")
+                .contentType(contentType)
+                .content(authJSON))
+                .andDo(print())
+                .andExpect(status().isConflict());
     }
 
+    @Test
+    @Sql(value = {"/test-set-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    public void signInOkTest() throws Exception {
+        Auth auth = new Auth();
+        auth.setEmail("exist@e.ru");
+        auth.setPassword("123");
+        this.mockMvc.perform(post("/users/auth")
+                .contentType(contentType)
+                .content(gson.toJson(auth)))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 
+    @Test
+    @Sql(value = {"/test-set-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    public void signInWrongCredentialsTest() throws Exception {
+        Auth auth = new Auth();
+        auth.setEmail("exist@e.ru");
+        auth.setPassword("1234");
+        this.mockMvc.perform(post("/users/auth")
+                .contentType(contentType)
+                .content(gson.toJson(auth)))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @Sql(value = {"/test-set-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    public void signInNotFoundUserTest() throws Exception {
+        Auth auth = new Auth();
+        auth.setEmail("notFoundUser@e.ru");
+        auth.setPassword("123");
+        this.mockMvc.perform(post("/users/auth")
+                .contentType(contentType)
+                .content(gson.toJson(auth)))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void logoutWithoutSessionTest() throws Exception {
+        //todo httpSession
+        MockHttpServletRequestBuilder builder =
+                MockMvcRequestBuilders.post("/users/logout").content("");
+        this.mockMvc.perform(builder).andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void infoOkTest() throws Exception {
+        //todo httpSession
+        Auth auth = new Auth();
+        auth.setEmail("exist@e.ru");
+        auth.setPassword("123");
+        Cookie[] allCookies = this.mockMvc.perform(post("/users/auth")
+                .contentType(contentType)
+                .content(gson.toJson(auth))).andReturn().getResponse().getCookies();
+        this.mockMvc.perform(get("/users/info")
+                .contentType(contentType)
+                .cookie(allCookies)
+        )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Sql(value = {"/test-set-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    public void changeOkTest() throws Exception {
+        User user = new User();
+//        user.set
+//        this.mockMvc.perform(post("/users/auth")
+//                .contentType(contentType)
+//                .content(gson.toJson(auth)))
+//                .andDo(print())
+//                .andExpect(status().isNotFound());
+    }
 }
